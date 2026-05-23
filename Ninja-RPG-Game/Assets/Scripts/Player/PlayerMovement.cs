d49ce24ed3d624170f8a493e3e5c1334211efdcd
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2026-05-23 14:07:05 -0400

    Updated Players Movements Configs

--- Ninja-RPG-Game/Assets/Scripts/Player/PlayerMovement.cs
+++ Ninja-RPG-Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,10 +6,33 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed;
 
     private PlayerActions actions;
+    private Rigidbody2D rb;
+    private Vector2 moveDirection;
 
     private void Awake()
     {
         actions = new PlayerActions();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        ReadMovement();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.MovePosition(rb.position + moveDirection * (speed * Time.fixedDeltaTime));
+    }
+
+    private void ReadMovement()
+    {
+        moveDirection = actions.Movement.Move.ReadValue<Vector2>().normalized;
     }
 
     private void OnEnable()
