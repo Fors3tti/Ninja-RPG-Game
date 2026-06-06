03ad39900de54ab682603c18cde69520d7083366
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2026-06-06 13:59:33 -0400

    Updated States System Configs

--- Ninja-RPG-Game/Assets/Scripts/Enemy/FSM/FSMState.cs
+++ Ninja-RPG-Game/Assets/Scripts/Enemy/FSM/FSMState.cs
@@ -7,16 +7,34 @@ public class FSMState
     public FSMAction[] Actions;
     public FSMTransition[] Transitions;
 
-    public void UpdateState()
+    public void UpdateState(EnemyBrain enemyBrain)
     {
         ExecuteActions();
+        ExecuteTransitions(enemyBrain);
     }
 
     private void ExecuteActions()
     {
-        for(int i = 0; i < Actions.Length; i++)
+        for (int i = 0; i < Actions.Length; i++)
         {
             Actions[i].Act();
+        }
+    }
+
+    private void ExecuteTransitions(EnemyBrain enemyBrain)
+    {
+        if (Transitions == null || Transitions.Length <= 0) return;
+        for (int i = 0; i < Transitions.Length; i++)
+        {
+            bool value = Transitions[i].Decision.Decide();
+            if (value)
+            {
+                enemyBrain.ChangeState(Transitions[i].TrueState);
+            }
+            else
+            {
+                enemyBrain.ChangeState(Transitions[i].FalseState);
+            }
         }
     }
 }
